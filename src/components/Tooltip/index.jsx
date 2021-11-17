13898b49a32d24d5018951446d2770c94495d8d3
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-11-18 00:40:08 +0530

    Add Tooltip component

---
+++ src/components/Tooltip/index.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import './style.scss';
+
+const Tooltip = ({
+    children, direction, delay, content,
+}) => {
+    let timeout;
+    const [active, setActive] = useState(false);
+
+    const showTip = () => {
+        timeout = setTimeout(() => {
+            setActive(true);
+        }, delay || 400);
+    };
+
+    const hideTip = () => {
+        clearInterval(timeout);
+        setActive(false);
+    };
+
+    return (
+        <div
+            className="Tooltip-Wrapper"
+            // When to show the tooltip
+            onMouseEnter={showTip}
+            onMouseLeave={hideTip}
+        >
+            {/* Wrapping */}
+            {children}
+            {active && (
+                <div className={`Tooltip-Tip ${direction || 'top'}`}>
+                    {/* Content */}
+                    {content}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Tooltip;
