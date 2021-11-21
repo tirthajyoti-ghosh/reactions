a6db4a5d1d451d70895172fdc31eaff61dbf6b15
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-11-21 10:25:11 +0530

    Add separate Content component

---
+++ src/components/Content/index.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable max-len */
+import Reactions from '../Reactions';
+
+function Content({ reactions, reactionCounts, handleReactionClick }) {
+    return (
+        <div>
+            <div style={{ width: '200px', height: '200px', backgroundColor: 'grey' }} />
+
+            <Reactions
+                reactions={reactions}
+                reactionCounts={reactionCounts}
+                onSelect={(reaction) => handleReactionClick(reaction)}
+            />
+        </div>
+    );
+}
+
+export default Content;
